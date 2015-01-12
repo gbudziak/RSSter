0272f4a792163375ee6ef8f70c911cfa2b01bf26
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-01-12 12:51:16 +0100

    First Commit

---
+++ RSSter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RSSter.Startup))]
+namespace RSSter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
