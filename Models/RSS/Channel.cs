9aca3ce2f2b7568c78b7163061cfecfb034096c7
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-01-13 11:32:54 +0100

    RSSreader implementation

---
+++ Models/RSS/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.RSS
+{
+    public class Channel
+    {
+        public List<Item> Items { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ RSSter/Controllers/RssReaderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using Models.RSS;
+
+namespace RSSter.Controllers
+{
+    public class RssReaderController : Controller
+    {
+
+        // GET: RssReader
+        public ActionResult RssListView()
+        {
+
+
+            const string _blogURL = "http://www.tvn24.pl/najnowsze.xml";
+            XDocument feedXml = XDocument.Load(_blogURL);
+            var feeds = from feed in feedXml.Descendants("item")
+               select new Item
+               {
+                   Title = feed.Element("title").Value,
+                   Link = feed.Element("link").Value,
+                   Description = Regex.Match(feed.Element("description").Value, @"^.{1,180}\b(?<!\s)").Value
+ 
+               };
+ 
+            return View();
+        }
+    }
+}
