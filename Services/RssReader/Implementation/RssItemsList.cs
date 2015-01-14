2be38178a60e18b496b65735ccc405d206106574
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-01-14 16:55:17 +0100

    Rss channel and list view

--- Services/RssReader/Implementation/RssItemsList.cs
+++ Services/RssReader/Implementation/RssItemsList.cs
@@ -18,43 +18,38 @@ namespace Services.RssReader.Implementation
 
         public Channel GetRssFeed(string blogUrl)
         {
-
-            //string xml;
-            //using (WebClient webClient = new WebClient())
-            //{
-            //    xml = Encoding.UTF8.GetString(webClient.DownloadData(blogUrl));
-            //}
-            //xml = xml.Replace("+00:00", "");
-            //byte[] bytes = UTF8Encoding.ASCII.GetBytes(xml);
-
-            //XmlReader reader = XmlReader.Create(new MemoryStream(bytes));
-            //SyndicationFeed feed = SyndicationFeed.Load(reader);
-
-
             XmlReader reader = XmlReader.Create(blogUrl);
-
 
             var feed = SyndicationFeed.Load(reader);
 
             var channelModel = new Channel{Link = blogUrl};
             List<Item> itemList = new List<Item>();
-            //var rssItem = new Item();
 
-            var ChannelDescription = feed.Description.Text;
-            var ChannelImage = feed.ImageUrl;
-
-            foreach (SyndicationItem item in feed.Items)
+            if (feed != null)
             {
-                var itemId = item.Id;
-                var itemPublishDate = item.PublishDate;
-                var itemLink = item.Links[0].Uri;
-                var itemSummary = item.Summary.Text;
-                var itemTitle = item.Title.Text;
-                //var itemCategory = item.Categories[0].Name;
+                var channelDescription = feed.Description.Text;
 
-                itemList.Add(new Item { Description = item.Summary.Text, Link = item.Links[0].Uri.ToString(), Title = item.Title.Text, PublishDate = item.PublishDate.ToString(), Category = "none" });
+                var channelImage = feed.ImageUrl;
+
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    var itemId = item.Id;
+                    var itemPublishDate = item.PublishDate;
+                    var itemLink = item.Links[0].Uri;
+                    var itemSummary = item.Summary.Text;
+                    var itemTitle = item.Title.Text;
+                    //var itemCategory = item.Categories[0].Name;
+
+                    itemList.Add(new Item
+                    {
+                        Description = item.Summary.Text,
+                        Link = item.Links[0].Uri.ToString(),
+                        Title = item.Title.Text,
+                        PublishDate = item.PublishDate.ToString(),
+                        Category = "none"
+                    });
+                }
             }
-
 
             //XDocument feedXml = XDocument.Load(blogUrl);
             //var feeds = feedXml.Descendants("item").Select(feed => new Item
@@ -64,6 +59,9 @@ namespace Services.RssReader.Implementation
             //    Description = Regex.Match(feed.Element("description").Value, @"^.{1,180}\b(?<!\s)").Value
             //});
             channelModel.Items = itemList;
+            channelModel.Description = feed.Description.Text;
+            channelModel.Image = feed.ImageUrl.ToString();
+            
             return channelModel;
         }
     }
