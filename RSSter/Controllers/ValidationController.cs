51833c2c95b0123088fc6e3231c6007878e0a6c1
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-01-19 11:31:50 +0100

    debug

--- RSSter/Controllers/ValidationController.cs
+++ RSSter/Controllers/ValidationController.cs
@@ -27,5 +27,24 @@ namespace RSSter.Controllers
         {
             return Json(_validateService.IsLinkValid(link), JsonRequestBehavior.AllowGet);
         }
+
+        //GET: Validation
+        public JsonResult LinkValidation(string link)
+        {
+            return Json(_validateService.IsLinkUnique(link), JsonRequestBehavior.AllowGet);
+        }
     }
 }
+
+
+        ////GET: Validation
+        //public JsonResult LinkValidation(string link)
+        //{            
+        //    return Json(_validateService.IsLinkUnique(link), JsonRequestBehavior.AllowGet);
+        //}
+
+        //public void ChannelValidation(string link)
+        //{
+        //    var isLinkUnique = _validateService.IsLinkUnique(link);
+            
+        //}
