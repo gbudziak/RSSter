9dbe21d38b667ac8a9f82f403f38e22233e3811f
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-01-21 11:20:45 +0100

    something

--- Context/ApplicationDbContext.cs
+++ Context/ApplicationDbContext.cs
@@ -44,9 +44,6 @@ namespace DBContext
 
         public DbSet<UserChannel> UserChannels { get; set; }
 
-        public new void SaveChanges()
-        {
-            base.SaveChanges();
-        }
+
     }
 }
