d00de6a434bd52f6e8dcd56d0220d629d81f0f1f
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-01-22 10:47:30 +0100

    automapper debug

--- RSSter/App_Start/MapperConfig.cs
+++ RSSter/App_Start/MapperConfig.cs
@@ -11,9 +11,10 @@ namespace RSSter
         {
             Mapper.CreateMap<SyndicationItem, Item>()
                 .ForMember(dest => dest.Description, opts => opts.MapFrom(item => item.Summary.Text))
+                .ForMember(x => x.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.Url, opts => opts.MapFrom(item => item.Links[0].Uri.ToString()))
                 .ForMember(dest => dest.PublishDate, opts => opts.MapFrom(item => item.PublishDate.ToString()))
-                .ForMember(dest => dest.Description, opts => opts.MapFrom(item => item.Summary.Text));
+                .ForMember(dest => dest.Title, opts => opts.MapFrom(item => item.Title.Text));
         }
     }
 }
