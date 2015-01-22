73347c7cd006feb1fcea0553a3e173b52928bd54
pawel85pgs <psolowczuk@pgs-soft.com>
2015-01-22 16:15:01 +0100

    minor changes

---
+++ RSSter/Scripts/isRead.js
@@ -0,0 +1,13 @@
+﻿function PostIsRead() {
+    var userItemId = $(this).attr("id");
+    $.ajax({
+        url: "/RssReader/Read/?userItemId=" + userItemId,
+        success: function (result) {
+            if (result) {
+                $("div#" + userItemId).slideToggle();
+            } else {
+                $("#Error").show();
+            }
+        }
+    });
+};
--- RSSter/Scripts/raitings.js
+++ RSSter/Scripts/raitings.js
@@ -1,6 +1,27 @@
 ﻿function RateUp() {
-    var userItemId = $("i.rateup").attr("id");
+    var userItemId = $(this).attr("id");
     $.ajax({
-        url: "/RssReader/RaitingUp/?userItemId=" + userItemId;
-})
-}
+        url: "/RssReader/RaitingUp/?userItemId=" + userItemId,
+        success: function(result) {
+            if (result) {
+                $("i#" + userItemId).fadeToggle();
+            } else {
+                $("#ratingError").show();
+            }
+        }
+    });
+};
+
+function RateDown() {
+    var userItemId = $(this).attr("id");
+    $.ajax({
+        url: "/RssReader/RaitingDown/?userItemId=" + userItemId,
+        success: function (result) {
+            if (result) {
+                $("i#" + userItemId).fadeToggle();
+            } else {
+                $("#ratingError").show();
+            }
+        }
+    });
+};
