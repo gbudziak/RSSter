25a4088aba0ada898b77a2dcfe369e62c79ff9d2
pawel85pgs <psolowczuk@pgs-soft.com>
2015-01-26 16:08:05 +0100

    Show/Hide read items functionality

--- RSSter/Scripts/Attachers/showUserItems.js
+++ RSSter/Scripts/Attachers/showUserItems.js
@@ -1,18 +1,15 @@
 ﻿/// <reference path="../jquery-2.1.3.intellisense.js" />
+/// <reference path="~/Scripts/Developer/isRead.js" />
+/// <reference path="~/Scripts/Developer/ratings.js" />
 $(function() {
     $(".up").on("click", RateUp);
     $(".down").on("click", RateDown);
     $(".hide").on("click", PostIsRead);
     $("a").on("click", MarkAsRead);
     $("i").each(RateIconsInitialization);
-    $("tr").each(HideEmoticons);
-    $('.left.sidebar')
-        .sidebar('attach events', '.toggle.button');
-
-    $('.sidebar')
-        .sidebar('attach events', '.toggle.thinbutton');
-
-    $('#unreadOrAll')
+    $("tr").each(HideEmoticons).each(HideReadItems); 
+    $(".ui.checkbox")
         .checkbox();
+    $("#unreadOrAll").on("change", CheckboxAllChange);
 
 })
