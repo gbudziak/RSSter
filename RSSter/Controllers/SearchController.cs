ef9f3a648ba5e4342b9b5dfc1efe8555ee653144
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-01-29 10:24:41 +0100

    commit

---
+++ RSSter/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSSter.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index(string search)
+        {
+
+            if (!String.IsNullOrEmpty(search))
+            {
+
+                //movies = movies.Where(s => s.Title.Contains(searchString));
+            } 
+
+            return View();
+        }
+    }
+}
