7e14af7d6a5639e1107d410fd3580fc5125433b7
gbudziak <grzegorz.marek.budziak@gmail.com>
2015-02-05 13:47:53 +0100

    views changes

---
+++ Models/ViewModels/ChannelItemsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PagedList;
+
+namespace Models.ViewModels
+{
+    public class ChannelItemsViewModel
+    {
+        public PagedList<CompleteItemInfo> Items { get; set; }
+
+        public long ChannelId { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public long Readers { get; set; }
+        public long TotalPosts { get; set; }
+        public double PostsPerDay { get; set; }
+        public string LastPost { get; set; }
+    }
+}
